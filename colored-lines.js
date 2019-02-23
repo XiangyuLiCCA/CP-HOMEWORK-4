64dbbb6732dacefd61da480fb288588175b35f2e
Xiangyu Li <47231543+XiangyuLiCCA@users.noreply.github.com>
2019-02-23 11:34:32 -0800

    Update colored-lines.js

--- colored-lines.js
+++ colored-lines.js
@@ -6,10 +6,10 @@ function setup() {
 function draw() {
   background(255);
 
-  for (var x = 10; x < width-5; x= x + 15) {
-    //x = 50 for Start in 50 pixels on left side
-    // x < width-50 for stop in 50 pixels on right side
-    // x= x + 5 means 5 pixels between each two points. This will affect the number of lines
+  for (var x = 10; x < width-5; x += 15) {
+    //x = 10 for Start in 50 pixels on left side
+    // x < width-5 for stop in 5 pixels on right side
+    // x += 15 means 15 pixels between each two points. This will affect the number of lines
     stroke(random(0, 300), 60, 200);
     line(x, height/2, mouseX, mouseY);
   }
