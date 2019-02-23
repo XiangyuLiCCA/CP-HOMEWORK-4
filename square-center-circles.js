c555c4a749180b8258090376e66c3021300c8595
Xiangyu Li <47231543+XiangyuLiCCA@users.noreply.github.com>
2019-02-23 12:08:35 -0800

    Update square-center-circles.js

--- square-center-circles.js
+++ square-center-circles.js
@@ -11,10 +11,13 @@ function draw() {
 
   x.push(mouseX); // equivalent to append(x, mouseX)
   y.push(mouseY); // equivalent to append(y, mouseY)
-
+ //it will make the ellipse follow the mouse moveing.
   for (var i = 0; i < x.length; i = i + 10) {
+     //if i = i + x so change x will affect the radius of each circle
     rectMode(CENTER)
+    //Align the center of each square
     square(x[i], y[i], 1 + (x.length - i));
+    //5 + (x.length - i) is the Side length of the square
   }
 
   x = x.slice(-90); // keep the last 90 x values
